1da3c27ac54a73ee5ff1777d396099a1536e80c6
Jim Flores <jimflores5@gmail.com>
2020-12-10 10:59:21 -0600

    Added comments and TODOs to the starter code.

--- hexcode.py
+++ hexcode.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/hex_form', methods=['GET', 'POST'])
+# Code the 'valid_hex_chars' function here:
+
+@app.route('/hex_form')
 def hex_form():    
     return render_template('hex_form.html')
 
 if __name__ == '__main__':
     app.run()
+
+# Find the exercise instructions here:
+# https://education.launchcode.org/lchs/chapters/flask-intro/exercises.html

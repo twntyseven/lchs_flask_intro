1d3a37680a487c38de548ec11774e15eb545a1cb
Jim Flores <jimflores5@gmail.com>
2021-06-20 11:23:28 -0500

    Added encrypt.py file to the project starter code.

---
+++ encrypt.py
@@ -0,0 +1,3 @@
+# Paste in the requested functions from your Coded Messages assignment.
+# If you didn't complete that assignment, use the code from the
+# LCHS-web-form-part-2 GitHub repository: https://github.com/LaunchCodeEducation/LCHS-web-form-part-2
